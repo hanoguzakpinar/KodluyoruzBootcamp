1bc771b49a5c8b08549c9a3af7e079d2ff94effe
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-14 15:27:18 +0300

    first commit

---
+++ console-programming/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminiz?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminiz?");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin {0} {1}",name,surname);
+        }
+    }
+}
