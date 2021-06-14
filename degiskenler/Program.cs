0864d63bd4eaa76344af578cb26aed3415a5ce61
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-14 16:16:40 +0300

    variables

--- degiskenler/Program.cs
+++ degiskenler/Program.cs
@@ -6,7 +6,23 @@ namespace degiskenler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Dünya!");
+            object a = 10;
+            object b = 'k';
+            object c = "metin";
+            object d = 12.9f;
+
+            bool b1 = true;
+            bool b2 = false;
+            bool b3 = 5 > 4;
+
+            char ac = 'g';
+            string bc = "deneme";
+            string cc = ac + bc + "Viki" + 'm';
+
+            int f = 10, g, m = 70;
+
+            var a = 5;
+            var b = "metin";
         }
     }
 }
