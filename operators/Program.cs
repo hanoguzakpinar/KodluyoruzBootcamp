fc4fcbfdff8a6f8218334263072a88f7d9a43f63
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-14 16:26:10 +0300

    operators

---
+++ operators/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atama ve İşlemli Atama Operatörleri (=, +=, -=, *=, /=)");
+            Console.WriteLine("Mantıksal Operatörler (||, &&, !)");
+            Console.WriteLine("İlişkisel Operatörler (==,!=, <, >, >=,<=)");
+            Console.WriteLine("Aritmetik (+, -, *, /, %, ++, --)");
+
+        }
+    }
+}
