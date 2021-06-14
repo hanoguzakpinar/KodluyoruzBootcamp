c68b9aa657876416327765d7af59af22dfd5e6f8
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-14 18:36:44 +0300

    v2

--- typeconversion/Program.cs
+++ typeconversion/Program.cs
@@ -6,7 +6,16 @@ namespace typeconversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float a; int b = 25; a = b;
+            short x = 10; int y; y = x;
+            int ax; ax = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ax);
+            int ac; ac = int.Parse(Console.ReadLine());
+            Console.WriteLine(ac);
+            long xq = long.Parse(Console.ReadLine());
+            Console.WriteLine(xq);
+            double sayi = 3.45; int ap = (int)sayi;
+            Console.WriteLine(sayi);
         }
     }
 }
