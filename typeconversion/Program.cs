99e2b6b344ea9b555459dd00017c7bd548492734
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-14 18:29:49 +0300

    v1

---
+++ typeconversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace typeconversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
