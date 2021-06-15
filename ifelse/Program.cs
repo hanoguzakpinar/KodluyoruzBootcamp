d9011c7f56087adeb77aafb37c20866e21edf007
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-15 13:52:01 +0300

    ifternary

---
+++ ifelse/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int saat = DateTime.Now.Hour;
+
+            if (saat>=6 && saat<11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if (saat>11 && saat<18)
+            {
+                Console.WriteLine("İyi Günler.");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler.");
+            }
+
+            string sonuc = saat>18 ? "İyi Geceler" : saat>=6 && saat<11 ? "Günaydın" : "İyi Günler";
+            Console.WriteLine(sonuc);
+
+        }
+    }
+}
