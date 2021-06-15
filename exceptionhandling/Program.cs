1f069766d0dc248dac9c29ffb44e2eaf068a9046
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-15 13:33:26 +0300

    exceptionhandling

---
+++ exceptionhandling/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace exceptionhandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+             {
+                 Console.WriteLine("Sayi giriniz");
+                 int sayi = Convert.ToInt32(Console.ReadLine());
+                 Console.WriteLine("Girilen Sayı: {0}",sayi);
+             }
+             catch (Exception ex)
+             {
+                 Console.WriteLine("Hata : {0}",ex.Message.ToString());
+
+             }
+             finally
+             {
+
+                 Console.WriteLine("İşlem Tamamlandı.");
+             }
+
+        //////////////////////////////////////////////////////////////////////
+
+        try
+        {
+            int number = int.Parse(null);
+        }
+        catch (ArgumentNullException msg)
+        {
+            Console.WriteLine("Hata : {0}",msg.Message.ToString()); 
+        }
+        catch(FormatException msg)
+        {
+            Console.WriteLine("Hata : {0}",msg.Message.ToString());
+        }
+        catch(OverflowException msg)
+        {
+            Console.WriteLine("Hata : {0}",msg.Message.ToString());
+        }
+        finally
+        {
+            Console.WriteLine("İşlem tamamlandı.");
+
+        }
+
+        }
+    }
+}
