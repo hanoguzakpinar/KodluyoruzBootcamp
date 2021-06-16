73c364a4f645cd3bae471f6b474c8249a1c21e84
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-16 11:38:56 +0300

    loop

---
+++ loops/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayi Gir:");
+            int sayi = int.Parse(Console.ReadLine());
+            for(int i=0;i<sayi;i++)
+            {
+                if(i%2==1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+            int ttoplam=0;
+            int ctoplam=0;
+
+            for(int i=1;i<=100;i++)
+            {
+                if(i%2==1)
+                {
+                    ttoplam +=i;
+                }
+            
+                if(i%2==0)
+                {
+                    ctoplam +=i;
+                }
+
+            }
+
+            Console.WriteLine("Tek Toplam :"+ttoplam);
+            Console.WriteLine("Çift Toplam :"+ctoplam);
+
+
+
+
+        }
+    }
+}
