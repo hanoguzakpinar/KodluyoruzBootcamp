bc97b83dee2fc1c372294def9966a52ce5061732
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-16 12:11:26 +0300

    array

---
+++ arrays/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eleman sayisi giriniz:");
+            int elm = int.Parse(Console.ReadLine());
+            int[] sayilar = new int[elm];
+
+            for(int i=0;i<sayilar.Length;i++)
+            {
+                Console.WriteLine("{0}. sayiyi giriniz :",i+1);
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var item in sayilar)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
