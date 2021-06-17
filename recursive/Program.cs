e744622e25cf4b5749079985a6ca3b77dc1fe09e
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-17 14:09:01 +0300

    recursive

---
+++ recursive/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayi:");
+            int sayi = int.Parse(Console.ReadLine());
+            Console.WriteLine("Üs:");
+            int us = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine(UsAl(sayi,us));
+
+        }
+
+        static int UsAl(int sayi , int us)
+        {
+            if(us<2)
+                return sayi;
+            
+            return UsAl(sayi,us-1)*sayi;
+        }
+    }
+}
