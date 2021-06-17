e589b50fea8210769986d36220d6763942cb1f2b
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-17 12:35:38 +0300

    overload

---
+++ methodoverload/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace methodoverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Islemler xx = new Islemler();
+
+            string sayi = "53";
+
+            bool sonuc = int.TryParse(sayi, out int CikanSayi);
+
+            if (sonuc)
+            {
+                xx.EkranaYaz("Başarılı");
+                xx.EkranaYaz(CikanSayi);
+            }
+            else
+            {
+                xx.EkranaYaz("Başarısız");
+            }
+
+        }
+    }
+
+    class Islemler
+    {
+        public void EkranaYaz(int sayi)
+        {
+            Console.WriteLine(sayi);
+        }
+
+        public void EkranaYaz(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+        
+
+    }
+}
