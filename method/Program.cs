cf311078241d40cebe61e650be904824c944ef08
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-17 11:37:39 +0300

    method

---
+++ method/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Birinci sayiyi gir:");
+            int s1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("İkinci sayiyi gir:");
+            int s2 = int.Parse(Console.ReadLine());
+
+            Islemler i = new Islemler();
+            int toplam = i.Topla(s1,s2);
+            int carp = i.Carp(s1,s2);
+
+            Console.WriteLine("Toplam:"+toplam);
+            Console.WriteLine("Carpma:"+carp);
+
+
+
+        }
+    }
+
+    class Islemler
+    {
+        public int Topla(int x,int y)
+        {
+            return x+y;
+        }
+
+        public int Carp(int x,int y)
+        {
+            return x*y;
+        }
+
+    }
+}
