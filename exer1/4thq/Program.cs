74136f853bdf6ba5837cdf9c0126b828c6597d43
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-17 18:13:19 +0300

    4thq

---
+++ exer1/4thq/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _4thq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.*/
+
+            Console.WriteLine("Cümle giriniz:");
+            string cumle = Console.ReadLine();
+
+            string[] w0rds = cumle.Split(' ');
+
+            Console.WriteLine("Kelime Sayısı:"+w0rds.Length);
+
+            string dnm = String.Join("",w0rds);
+
+            Console.WriteLine("Harf Sayısı:"+dnm.Length);
+
+        }
+    }
+}
