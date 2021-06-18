44eff70d99952a95b7b5e62a66867361d7edb0f1
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-18 21:46:55 +0300

    arrayL

---
+++ arrayList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            // list.Add("Name");
+            // list.Add(96);
+            // list.Add(true);
+            // list.Add('C');
+
+            // Console.WriteLine(list[2]);
+
+            // foreach (var item in list)
+            //     Console.WriteLine(item);
+
+
+            Console.WriteLine("***************");
+            //AddRange - Eleman Ekleme Collection Türünde.
+
+            // string[] harf = {"ABC","DEF","GGX","XYZ"};
+            List<int> nmr = new List<int>() { 0, 5, 6, 7, 8, 9 };
+            // list.AddRange(harf);
+            list.AddRange(nmr);
+
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            // Sort - Sıralama - Aynı Tür Elemanlar
+            Console.WriteLine("***************");
+            list.Sort();
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+
+            // Binary Search - Önce Sıralama Yapılması Gerekli
+            Console.WriteLine("***************");
+            Console.WriteLine(list.BinarySearch(5));
+
+
+            // Reverse
+            Console.WriteLine("***************");
+            list.Reverse();
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+            // Clear
+            Console.WriteLine("***************");
+            list.Clear();
+            foreach (var item in list)
+                Console.WriteLine(item);
+
+
+
+
+        }
+    }
+}
