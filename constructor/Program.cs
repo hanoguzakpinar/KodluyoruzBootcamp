07097b078850b8597cc784fd279e968ef0394f8d
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-19 14:58:39 +0300

    encapsul

--- constructor/Program.cs
+++ constructor/Program.cs
@@ -6,32 +6,69 @@ namespace constructor
     {
         static void Main(string[] args)
         {
-            Users u1 = new Users(53,"OG","Han","hanOG");
-
+            Users u1 = new Users(53, "OG", "Han", 2);
             u1.Yazdir();
+
+            u1.SinifDusur();
+            u1.Yazdir();
+
+            u1.SinifDusur();
+            u1.Yazdir();
+
+            u1.SinifYukselt();
+            u1.Yazdir();
+            
         }
     }
 
     class Users
     {
-        public int No;
-        public string Ad;
-        public string Soyad;
-        public string UName;
+        private int _no;
+        private string _ad;
+        private string _soyad;
+        private int _sinif;
 
-        public Users(int no, string ad, string soyad, string uname)
+        public int No { get => _no; set => _no = value; }
+        public string Ad { get => _ad; set => _ad = value; }
+        public string Soyad { get => _soyad; set => _soyad = value; }
+        public int Sinif
         {
-            this.No = no;
-            this.Ad = ad;
-            this.Soyad = soyad;
-            this.UName =uname;
-        }
-        
-        public Users(){}
+            get => _sinif;
+            set
+            {
+                if (value< 1)
+                {
+                    Console.WriteLine("Sınıf 1'den az olamaz.");
+                    this._sinif = 1;
+                }
+                else
+                    this._sinif = value;
 
+            }
+
+        }
+
+        public Users(int no, string ad, string soyad, int sinif)
+        {
+            _no = no;
+            _ad = ad;
+            _soyad = soyad;
+            _sinif = sinif;
+        }
+
+        public Users() { }
         public void Yazdir()
         {
-            Console.WriteLine("İsim: {0} Soyisim: {1} No: {2} Username: {3}",Ad,Soyad,No,UName);
+            Console.WriteLine("İsim: {0} Soyisim: {1} No: {2} Sınıf: {3}", Ad, Soyad, No, Sinif);
+        }
+
+        public void SinifDusur()
+        {
+            this.Sinif = this.Sinif - 1;
+        }
+        public void SinifYukselt()
+        {
+            this.Sinif = this.Sinif + 1;
         }
 
     }
