692601088661fe968c6013847a4fd5b2087df513
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-06-19 13:48:03 +0300

    clss

---
+++ constructor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Users u1 = new Users(53,"OG","Han","hanOG");
+
+            u1.Yazdir();
+        }
+    }
+
+    class Users
+    {
+        public int No;
+        public string Ad;
+        public string Soyad;
+        public string UName;
+
+        public Users(int no, string ad, string soyad, string uname)
+        {
+            this.No = no;
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.UName =uname;
+        }
+        
+        public Users(){}
+
+        public void Yazdir()
+        {
+            Console.WriteLine("İsim: {0} Soyisim: {1} No: {2} Username: {3}",Ad,Soyad,No,UName);
+        }
+
+    }
+}
