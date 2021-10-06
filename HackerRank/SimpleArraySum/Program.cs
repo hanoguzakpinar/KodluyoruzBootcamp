bb021affc0358e90bcea97ad248507f0bf11831c
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-06 11:23:49 +0300

    simpleArraySum

---
+++ HackerRank/SimpleArraySum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+class Result
+{
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sum=0;
+        for(int i=0;i<ar.Count;i++)
+        {
+            sum += ar[i];
+        }
+        return sum;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
