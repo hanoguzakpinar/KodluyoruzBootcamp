f5a9344b2dc821c6bdef0776a634ccd0cf7d074d
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-06 18:53:04 +0300

    pangrams

---
+++ HackerRank/Pangrams/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System;
+
+class Result
+{
+    public static string pangrams(string s)
+    {
+        char[] alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+        char[] value = s.ToCharArray();
+
+        int pangram = 0;
+
+        for (int i = 0; i < alpha.Length; i++)
+        {
+            for (int j = 0; j < value.Length; j++)
+            {
+                if (char.ToLower(alpha[i]) == char.ToLower(value[j]))
+                {
+                    pangram++;
+                    break;
+                }
+            }
+        }
+
+        if (pangram == 26)
+            return "pangram";
+        else
+            return "not pangram";
+    }
+}
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        string result = Result.pangrams(s);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
