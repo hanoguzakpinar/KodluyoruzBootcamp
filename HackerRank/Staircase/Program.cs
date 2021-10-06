8396c5ebd00cce05f11f4f1416ce8dbf947ae8d5
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-10-06 12:09:26 +0300

    stairCase

---
+++ HackerRank/Staircase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Result
+{
+    public static void staircase(int n)
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (j >= i)
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(' ');
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
