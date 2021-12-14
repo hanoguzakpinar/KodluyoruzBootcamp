1600db4767b59ec32edf801f2d135eafcc1d9d4a
Oğuzhan <oguzhanakpinar@yahoo.com>
2021-12-14 12:52:17 +0300

    js hw1

---
+++ Frontend/js/hw1/js/clock.js
@@ -0,0 +1,41 @@
+let fullName = prompt("İsminizi giriniz.");
+
+let myName = document.querySelector("#myName");
+myName.innerHTML = fullName;
+
+function showTime() {
+    let myClock = document.querySelector("#myClock");
+
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    let day;
+    switch (new Date().getDay()) {
+        case 0:
+            day = " Pazar";
+            break;
+        case 1:
+            day = " Pazartesi";
+            break;
+        case 2:
+            day = " Salı";
+            break;
+        case 3:
+            day = " Çarşamba";
+            break;
+        case 4:
+            day = " Perşembe";
+            break;
+        case 5:
+            day = " Cuma";
+            break;
+        case 6:
+            day = " Cumartesi";
+    }
+
+    myClock.innerHTML = time + day;
+
+    setInterval(showTime, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', showTime, false);
