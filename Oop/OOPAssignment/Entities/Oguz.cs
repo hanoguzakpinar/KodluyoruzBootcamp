1610c334af0f3e75da410636b29e640c288cdd64
Deniz Duman <ce.denizduman2@gmail.com>
2022-03-22 15:49:49 +0300

    review alabilir miyim?

---
+++ Oop/OOPAssignment/Entities/Oguz.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPAssignment.Entities
+{
+    class Oguz
+    {
+        public int ID { get; set; } = 53;
+    }
+}
